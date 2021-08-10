3a66991ccc5a74aeef057f949839eacf6347f366
saroshkhandev <saroshabdullah2013@gmail.com>
2021-08-10 20:58:53 +0530

    multiple slices

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { Fragment } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import Header from "./components/Header";
 import Counter from "./components/Counter";
 import UserProfile from "./components/UserProfile";
 import Auth from "./components/Auth";
-import { authActions } from "./store/index";
 
 function App() {
-  const dispatch = useDispatch();
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   console.log(isAuthenticated);
   return (
